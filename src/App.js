4d8b272af8091f7ad70c23483d6461b6592ffd7a
Julia Tran <julia.kim.tran@hotmail.com>
2022-09-22 14:04:02 -0400

    Add react router

--- src/App.js
+++ src/App.js
@@ -5,30 +5,22 @@ import './styles.css';
 import About from './pages/About';
 import Pricing from './pages/Pricing';
 import Home from './pages/Home';
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
-  let component
-  switch (window.location.pathname) {
-    case '/home' :
-      component = <Home />
-    break
-    case '/pricing' :
-      component = <Pricing />
-      break
-      case '/about' :
-        component = <About />
-        break
-
-  }
 
 
   return (
-    <div className="App">
+    <>
       <Navbar />
        <div className ='container'>
-        {component}
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/pricing' element={<Pricing />} />
+          </Routes>
        </div>
-    </div>
+    </>
   );
 }
 
